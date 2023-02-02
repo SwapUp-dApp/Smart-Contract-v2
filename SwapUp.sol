f825816ae2e722d7fba62e7e56419e5aac99aacf
helloibm <butshikan@gmail.com>
2023-02-02 18:07:19 +0500

    backup files added

--- SwapUp.sol
+++ SwapUp.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.4.17 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-contract Swap {
+contract SwapUp {
   constructor() {}
 
   function transfer(bytes[] memory nfts, address init, address accept) public{
