2e72b66f3cbc0518bf36c596eca35d94f039a356
Ervin Abliamitov <er.abliamitov@gmail.com>
2024-05-16 11:21:14 +0300

    swap contract 1.0

--- contracts/SwapUp.sol
+++ contracts/SwapUp.sol
@@ -1,21 +1,191 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/utils/introspection/ERC165Checker.sol";
+import '@openzeppelin/contracts/token/ERC721/IERC721.sol';
+import '@openzeppelin/contracts/token/ERC1155/IERC1155.sol';
+import '@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol';
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import '@openzeppelin/contracts/utils/introspection/ERC165Checker.sol';
+import '@openzeppelin/contracts/utils/cryptography/ECDSA.sol';
+import '@openzeppelin/contracts/utils/cryptography/EIP712.sol';
 
-contract SwapUp {
-    // Event emitted when a token transfer occurs
-    event TokenTransferred(address indexed tokenAddress, address indexed sender, address indexed receiver, uint256 amount);
-    event NFTTransferred(address indexed nftAddress, address indexed sender, address indexed receiver, uint256 tokenId);
-
+contract SwapUp is EIP712 {
     using ERC165Checker for address;
 
     bytes4 private constant INTERFACE_ID_ERC721 = 0x80ac58cd;
     bytes4 private constant INTERFACE_ID_ERC1155 = 0xd9b67a26;
+
+    // Event emitted when a token transfer occurs
+    event TokenTransferred(
+        address indexed assetAddress,
+        address indexed party1Address,
+        address indexed party2Address,
+        uint256 amount
+    );
+    event NFTTransferred(
+        address indexed assetAddress,
+        address indexed party1Address,
+        address indexed party2Address,
+        uint256 tokenId
+    );
+    event SwapCreated(string swapId, address inititator, address responder);
+    event SwapCompleted(string swapId, address inititator, address responder);
+    event LogEvent(string text);
+
+    constructor() EIP712('SwapUp', '1') {}
+
+    struct Asset {
+        address assetAddress;
+        // can be either amount or NFT identifier
+        uint256 value;
+    }
+
+    struct Swap {
+        string swapId;
+        address initiatorAddress;
+        Asset[] initiatorAssets;
+        address responderAddress;
+        Asset[] responderAssets;
+        bool initiatorApprove;
+        bool responderApprove;
+        string status; // PENDING, COMPLETED, REJECT
+        string swapType; // OPEN, PRIVATE
+    }
+
+    mapping(string => Swap) public swaps;
+
+    function approveAndSwap(
+        string calldata swapId,
+        address responderAddress,
+        Asset[] calldata initiatorAssets,
+        Asset[] calldata responderAssets,
+        string calldata swapType
+    ) public {
+        Swap storage targetSwap = swaps[swapId];
+
+        require(responderAddress != address(0), 'invalid initiator address');
+        require(initiatorAssets.length > 0, 'no initiator assets found');
+        require(responderAssets.length > 0, 'no contragent assets found');
+
+        if (bytes(targetSwap.swapId).length > 0) {
+            require(
+                msg.sender == targetSwap.responderAddress,
+                'only responder is allowed to proceed with this swap'
+            );
+            require(
+                keccak256(bytes(targetSwap.status)) ==
+                    keccak256(bytes('PENDING')),
+                'Swap is not pending'
+            );
+            require(
+                initiatorAssets.length == targetSwap.initiatorAssets.length,
+                "Provided assets don't match to the initial swap setup"
+            );
+            require(
+                responderAssets.length == targetSwap.responderAssets.length,
+                "Provided assets don't match to the initial swap setup"
+            );
+
+            _validateAssetsBeforeTransfer(
+                targetSwap.initiatorAssets,
+                initiatorAssets
+            );
+            _validateAssetsBeforeTransfer(
+                targetSwap.responderAssets,
+                responderAssets
+            );
+
+            for (uint i = 0; i < targetSwap.initiatorAssets.length; i++) {
+                _transferAssets(
+                    targetSwap.initiatorAddress,
+                    targetSwap.responderAddress,
+                    targetSwap.initiatorAssets[i].assetAddress,
+                    targetSwap.initiatorAssets[i].value
+                );
+            }
+
+            for (uint i = 0; i < targetSwap.responderAssets.length; i++) {
+                _transferAssets(
+                    targetSwap.responderAddress,
+                    targetSwap.initiatorAddress,
+                    targetSwap.responderAssets[i].assetAddress,
+                    targetSwap.responderAssets[i].value
+                );
+            }
+
+            targetSwap.responderApprove = true;
+            targetSwap.status = 'COMPLETED';
+
+            emit SwapCompleted(
+                swapId,
+                targetSwap.initiatorAddress,
+                targetSwap.responderAddress
+            );
+        } else {
+            require(bytes(swapType).length > 0, 'Swap type is missing');
+            require(
+                keccak256(bytes(swapType)) == keccak256(bytes('OPEN')) ||
+                    keccak256(bytes(swapType)) == keccak256(bytes('PRIVATE')),
+                'Incorrect swapType provided'
+            );
+
+            targetSwap.swapId = swapId;
+            targetSwap.initiatorAddress = msg.sender;
+            targetSwap.responderAddress = responderAddress;
+            targetSwap.initiatorApprove = true;
+            targetSwap.responderApprove = false;
+            targetSwap.status = 'PENDING';
+            targetSwap.swapType = swapType;
+
+            for (uint i = 0; i < initiatorAssets.length; i++) {
+                targetSwap.initiatorAssets.push(
+                    Asset({
+                        assetAddress: initiatorAssets[i].assetAddress,
+                        value: initiatorAssets[i].value
+                    })
+                );
+            }
+
+            for (uint i = 0; i < responderAssets.length; i++) {
+                targetSwap.responderAssets.push(
+                    Asset({
+                        assetAddress: responderAssets[i].assetAddress,
+                        value: responderAssets[i].value
+                    })
+                );
+            }
+
+            emit SwapCreated(
+                swapId,
+                targetSwap.initiatorAddress,
+                targetSwap.responderAddress
+            );
+        }
+    }
+
+    function _validateAssetsBeforeTransfer(
+        Asset[] storage swapPartyAssets,
+        Asset[] calldata actualPartyAssets
+    ) private view {
+        require(swapPartyAssets.length > 0, 'No assets provided');
+        require(actualPartyAssets.length > 0, 'No assets provided');
+        require(
+            actualPartyAssets.length == swapPartyAssets.length,
+            'Invalid assets provided'
+        );
+
+        for (uint i = 0; i < actualPartyAssets.length; i++) {
+            require(
+                swapPartyAssets[i].assetAddress ==
+                    actualPartyAssets[i].assetAddress,
+                'asset address is incorrect'
+            );
+            require(
+                swapPartyAssets[i].value == actualPartyAssets[i].value,
+                'asset value is incorrect'
+            );
+        }
+    }
 
     // Function to swap tokens from the contract address to the recipient address
     function transferTokens(
@@ -24,13 +194,13 @@ contract SwapUp {
         address tokenAddress,
         uint256 amount
     ) public {
-        require(senderAddress != address(0), "Invalid sender address");
-        
+        require(senderAddress != address(0), 'Invalid sender address');
+
         // Check if receiver address is not zero
-        require(recipient != address(0), "Invalid receiver address");
-        
+        require(recipient != address(0), 'Invalid receiver address');
+
         // Check if amount is not zero
-        require(amount > 0, "Invalid amount");
+        require(amount > 0, 'Invalid amount');
 
         // initiate safe transfer
         _safeTransferFrom(tokenAddress, senderAddress, recipient, amount);
@@ -40,33 +210,41 @@ contract SwapUp {
         address senderAddress,
         address recipient,
         address nftAddress,
-        uint256 tokenId,
-        uint256 quantity
+        uint256 tokenId
     ) public {
-        require(senderAddress != address(0), "Invalid sender address");
-        require(recipient != address(0), "Invalid receiver address");
+        require(senderAddress != address(0), 'Invalid sender address');
+        require(recipient != address(0), 'Invalid receiver address');
 
         if (_isERC721(nftAddress)) {
             IERC721 nft = IERC721(nftAddress);
-
-            require(nft.ownerOf(tokenId) == senderAddress, "Sender must be the owner of the token");
+            require(
+                nft.getApproved(tokenId) == address(this),
+                'Token not approved for transfer'
+            );
 
             // Transfer the NFT
-            nft.transferFrom(senderAddress, recipient, tokenId);
-            
-            // Emit the NFT transfer event
-            emit NFTTransferred(nftAddress, senderAddress, recipient, tokenId);
+            nft.safeTransferFrom(senderAddress, recipient, tokenId);
         } else if (_isERC1155(nftAddress)) {
             IERC1155 nft = IERC1155(nftAddress);
 
-            require(nft.balanceOf(senderAddress, tokenId) >= quantity, "Insufficient balance");
-
-            nft.safeTransferFrom(senderAddress, recipient, tokenId, quantity, "");
-
-            // Emit the NFT transfer event
-            emit NFTTransferred(nftAddress, senderAddress, recipient, tokenId);
+            nft.safeTransferFrom(senderAddress, recipient, tokenId, 1, '');
         }
-        
+
+        // Emit the NFT transfer event
+        emit NFTTransferred(nftAddress, senderAddress, recipient, tokenId);
+    }
+
+    function _transferAssets(
+        address party1,
+        address party2,
+        address assetAddress,
+        uint256 assetValue
+    ) internal {
+        if (_isERC721(assetAddress) || _isERC1155(assetAddress)) {
+            transferNFT(party1, party2, assetAddress, assetValue);
+        } else {
+            transferTokens(party1, party2, assetAddress, assetValue);
+        }
     }
 
     function _safeTransferFrom(
@@ -78,10 +256,10 @@ contract SwapUp {
         IERC20 token = IERC20(tokenAddress);
 
         // Check if the sender has enough balance
-        require(token.balanceOf(sender) >= amount, "Insufficient balance");
+        require(token.balanceOf(sender) >= amount, 'Insufficient balance');
 
         token.approve(address(this), amount);
-                
+
         token.transferFrom(sender, recipient, amount);
     }
 
--- test/swapUp.test.js
+++ test/swapUp.test.js
@@ -1,6 +1,8 @@
 const SwapUp = artifacts.require('SwapUp');
 const MockERC20 = artifacts.require('MockERC20'); // Ensure you have a MockERC20 contract.
 const TestNFT = artifacts.require('TestNFT');
+const { expectRevert } = require('@openzeppelin/test-helpers');
+
 
 contract("test block", (accounts) => {
     let contract;
@@ -8,7 +10,7 @@ contract("test block", (accounts) => {
     let nft;
     const sender = accounts[1]; // Using a different account as sender
     const recipient = accounts[2];
-    const tokenAmount = web3.utils.toBN(10000000);
+    const tokenAmount = '10000000000000000000';
 
     before(async () => {
         contract = await SwapUp.deployed();
@@ -18,8 +20,22 @@ contract("test block", (accounts) => {
         // Setup: allocate tokens to sender
         await token.mint(sender, tokenAmount);
 
+        // INITIAL NFT IDs per sender: 0-3, per recipient: 4-7.
+        await nft.mintTo(sender);
+        await nft.mintTo(sender);
+        await nft.mintTo(sender);
+        await nft.mintTo(sender);
+
+        await nft.mintTo(recipient);
+        await nft.mintTo(recipient);
+        await nft.mintTo(recipient);
+        await nft.mintTo(recipient);
+
         // Setup: approve the SwapUp contract to spend tokens on behalf of sender
         await token.approve(contract.address, tokenAmount, {from: sender});
+
+        await nft.setApprovalForAll(contract.address, true);
+
     });
 
     it("deploys", async () => {
@@ -31,10 +47,97 @@ contract("test block", (accounts) => {
         assert.equal(isTransactionSuccess.receipt.status, true);
     });
 
-    // it("nft swap - test transaction", async () => {
+    it("test nft is minted", async () => {
+        const nextNftIndex = await nft.nextTokenId();
 
-    //     const txn = await debug(contract.transferNFT(sender, recipient, nft.address, 0, { from: sender }));
+        assert(nextNftIndex.toNumber() > 0, "NFT did not mint")
+    })
 
-    //     assert.equal(txn.receipt.status, true);
-    // })
+    // SWAP Sender's 1st NFT into RECPIENT 1st NFT (0 -> 4 | 4 -> 0)
+    it("swap single NFT between user A and user B", async () => {
+        await nft.approve(contract.address, 0, { from: sender });
+        await nft.approve(contract.address, 4, { from: recipient });
+
+        const ownerOf1NFT = await nft.ownerOf(0);
+        const ownerOf5NFT = await nft.ownerOf(4);
+
+        assert.equal(ownerOf1NFT, sender, 'invalid owner of 1st NFT');
+        assert.equal(ownerOf5NFT, recipient, 'invalid owner of 2nd NFT');
+
+        const tx = await contract.approveAndSwap('testID-1', recipient, [{ assetAddress: nft.address, value: 0 }], [{ assetAddress: nft.address, value: 4}], 'PRIVATE', { from: sender });
+
+        assert.equal(tx.receipt.status, true, 'status of transaction is false');
+
+        const swapTx = await contract.approveAndSwap('testID-1', recipient, [{ assetAddress: nft.address, value: 0 }], [{ assetAddress: nft.address, value: 4}], 'PRIVATE', { from: recipient });
+
+        assert.equal(swapTx.receipt.status, true, 'status of swap tx is false');
+
+        const ownerOf1NFTUpd = await nft.ownerOf(0);
+        const ownerOf5NFTUpd = await nft.ownerOf(4);
+
+        assert.equal(ownerOf1NFTUpd, recipient, 'invalid owner of 1st NFT after transfer');
+        assert.equal(ownerOf5NFTUpd, sender, 'invalid owner of 2nd NFT after transfer');
+    });
+
+    it("swap multiple NFTs between user A and user B", async () => {
+        await nft.approve(contract.address, 1, { from: sender });
+        await nft.approve(contract.address, 2, { from: sender });
+        await nft.approve(contract.address, 5, { from: recipient });
+        await nft.approve(contract.address, 6, { from: recipient });
+
+        const ownerOf2NFT = await nft.ownerOf(1);
+        const ownerOf3NFT = await nft.ownerOf(2);
+        const ownerOf6NFT = await nft.ownerOf(5);
+        const ownerOf7NFT = await nft.ownerOf(6);
+
+        assert.equal(ownerOf2NFT, sender, 'invalid owner of 2nd NFT');
+        assert.equal(ownerOf3NFT, sender, 'invalid owner of 2nd NFT');
+        assert.equal(ownerOf6NFT, recipient, 'invalid owner of 1st NFT');
+        assert.equal(ownerOf7NFT, recipient, 'invalid owner of 2nd NFT');
+
+        const tx = await contract.approveAndSwap('testID-2', sender, [{ assetAddress: nft.address, value: 0 }, { assetAddress: nft.address, value: 2 }], [{ assetAddress: nft.address, value: 1}, { assetAddress: nft.address, value: 3 }], 'PRIVATE', { from: recipient });
+
+        assert.equal(tx.receipt.status, true, 'status of transaction is false');
+
+        const swapTx = await contract.approveAndSwap('testID-', sender, [{ assetAddress: nft.address, value: 0 }, { assetAddress: nft.address, value: 2 }], [{ assetAddress: nft.address, value: 1}, { assetAddress: nft.address, value: 3 }], 'PRIVATE', { from: sender });
+
+        assert.equal(swapTx.receipt.status, true, 'status of swap tx is false');
+
+        const ownerOf2NFTUpd = await nft.ownerOf(1);
+        const ownerOf3NFTUpd = await nft.ownerOf(2);
+        const ownerOf6NFTUpd = await nft.ownerOf(5);
+        const ownerOf7NFTUpd = await nft.ownerOf(6);
+
+        assert.equal(ownerOf2NFTUpd, sender, 'invalid owner of 2nd NFT');
+        assert.equal(ownerOf3NFTUpd, sender, 'invalid owner of 2nd NFT');
+        assert.equal(ownerOf6NFTUpd, recipient, 'invalid owner of 1st NFT');
+        assert.equal(ownerOf7NFTUpd, recipient, 'invalid owner of 2nd NFT');
+    });
+
+    it("swap single NFT from user A to user B | and crypto currency from user B to user A", async () => {
+        await nft.approve(contract.address, 0, { from: recipient });
+
+        const ownerOf1NFT = await nft.ownerOf(0);
+
+        await token.mint(sender, '1000000000000000000000');
+        await token.approve(contract.address, '1000000000000000000000', { from: sender });
+
+        const balance = await token.balanceOf(sender);
+
+        assert.equal(ownerOf1NFT, recipient, 'invalid owner of 1st NFT');
+        assert.equal(balance.toString(), '1000000000000000000000', 'Incorrect balance value after minting token for sender adress');
+
+        const tx = await contract.approveAndSwap('testID-4', sender, [{ assetAddress: nft.address, value: 0 }], [{ assetAddress: token.address, value: '10000000000000000000'}], 'PRIVATE', { from: recipient });
+
+        assert.equal(tx.receipt.status, true, 'status of transaction is false');
+
+
+        const swapTx = await contract.approveAndSwap('testID-4', sender, [{ assetAddress: nft.address, value: 0 }], [{ assetAddress: token.address, value: '10000000000000000000'}], 'PRIVATE', { from: sender });
+
+        assert.equal(swapTx.receipt.status, true, 'status of swap tx is false');
+
+        const ownerOf1NFTUpd = await nft.ownerOf(0);
+
+        assert.equal(ownerOf1NFTUpd, sender, 'invalid owner of 1st NFT after transfer');
+    });
 });
