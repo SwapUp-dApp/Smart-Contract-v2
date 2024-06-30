6c3f54d2f218c538222db8001a4f593b0ae8c4a3
ibm <butshikan@gmail.com>
2024-06-30 17:49:17 +0500

    modified smart contract for June30th build

--- contracts/SwapUp.sol
+++ contracts/SwapUp.sol
@@ -10,10 +10,13 @@ import '@openzeppelin/contracts/utils/introspection/ERC165Checker.sol';
 import '@openzeppelin/contracts/utils/cryptography/ECDSA.sol';
 import '@openzeppelin/contracts/utils/cryptography/EIP712.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
+import '@openzeppelin/contracts/utils/Strings.sol';
 import '@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol';
+
 
 contract SwapUp is EIP712, Ownable {
     using ERC165Checker for address;
+    using Strings for string;
 
     bytes4 private constant INTERFACE_ID_ERC721 = 0x80ac58cd;
     bytes4 private constant INTERFACE_ID_ERC1155 = 0xd9b67a26;
@@ -34,8 +37,9 @@ contract SwapUp is EIP712, Ownable {
         Asset[] responderAssets;
         bool initiatorApprove;
         bool responderApprove;
-        string status; // PENDING, COMPLETED, REJECT
+        string status; // PENDING, COMPLETED, REJECTED, (COUNTER will be treated as PENDING for simplicity)
         string swapType; // OPEN, PRIVATE
+        uint256 platformFee;
     }
 
     mapping(string => Swap) public swaps;
@@ -58,11 +62,15 @@ contract SwapUp is EIP712, Ownable {
         address indexed party2Address,
         uint256 tokenId
     );
+
     event SwapCreated(string swapId, address initiator, address responder);
-    event SwapCompleted(string swapId, address initiator, address responder);
+    event SwapCountered(string swapId, address initiator, address responder);
+    event SwapCompleted(string swapId, address initiator, address responder, string status);
+
     event CommissionUpdated(string commissionType, uint256 amount);
     event AddressUpdated(string addressType, address newAddress);
 
+    // 
     constructor(
         address initialOwner,
         address _treasuryWalletAddress,
@@ -110,155 +118,215 @@ contract SwapUp is EIP712, Ownable {
         );
     }
 
-    function approveAndSwap(
+    //create a new swap and save the state. deposit the platform fee
+    function createSwap(
         string calldata swapId,
         address responderAddress,
-        Asset[] calldata initiatorAssets,
-        Asset[] calldata responderAssets,
-        string calldata swapType
+        Asset[] memory initiatorAssets,
+        Asset[] memory responderAssets,
+        string memory swapType
     ) public payable {
-        Swap storage targetSwap = swaps[swapId];
-
+        //Create a new swap and verify it does not exist
+        Swap storage newSwap = swaps[swapId];
+        require(bytes(newSwap.swapId).length == 0, 'A swap with this id already exists');
+        
         require(responderAddress != address(0), 'Invalid responder address');
         require(initiatorAssets.length > 0, 'No initiator assets found');
         require(responderAssets.length > 0, 'No responder assets found');
 
-        // Calculate the equivalent ETH amount for $10 using Chainlink price feed
-        uint256 ethAmountForPlatformFee = getFeeInETH();
+        require(bytes(swapType).length > 0, 'Swap type is missing');
+        require(swapType.equal('OPEN') || swapType.equal('PRIVATE'), "Swap Type is invalid");
 
+        // Calculate the equivalent ETH amount of the platform fee using Chainlink price feed
+        uint256 ethAmountForPlatformFee = getFeeInETH();
+        
         // Ensure both parties have provided enough ETH for the platform fee
         require(
             msg.value >= ethAmountForPlatformFee,
             'Insufficient ETH for platform fee'
         );
 
-        // Transfer platform fee from responder to the contract
+        newSwap.swapId = swapId;
+        newSwap.initiatorAddress = msg.sender;
+        newSwap.responderAddress = responderAddress;
+        newSwap.initiatorApprove = true;
+        newSwap.responderApprove = false;
+        newSwap.status = 'PENDING';
+        newSwap.swapType = swapType;
+        newSwap.platformFee = ethAmountForPlatformFee;
+       
+        for (uint i = 0; i < initiatorAssets.length; i++) {
+            newSwap.initiatorAssets.push(
+                Asset({
+                    assetAddress: initiatorAssets[i].assetAddress,
+                    value: initiatorAssets[i].value
+                })
+            );
+        }
+        for (uint i = 0; i < responderAssets.length; i++) {
+            newSwap.responderAssets.push(
+                Asset({
+                    assetAddress: responderAssets[i].assetAddress,
+                    value: responderAssets[i].value
+                })
+            );
+        }
+        
+        // TODO: Transfer platform fee from responder to the contract at the end when the function call is complete
         payable(treasuryWalletAddress).transfer(msg.value);
 
-        if (bytes(targetSwap.swapId).length > 0) {
-            require(
-                msg.sender == targetSwap.responderAddress,
-                'Only responder is allowed to proceed with this swap'
-            );
-            require(
-                keccak256(bytes(targetSwap.status)) ==
-                    keccak256(bytes('PENDING')),
-                'Swap is not pending'
-            );
-            require(
-                initiatorAssets.length == targetSwap.initiatorAssets.length,
-                "Provided assets don't match the initial swap setup"
-            );
-            require(
-                responderAssets.length == targetSwap.responderAssets.length,
-                "Provided assets don't match the initial swap setup"
-            );
-            require(
-                keccak256(bytes(targetSwap.swapType)) ==
-                    keccak256(bytes(swapType)),
-                'Incorrect swapType provided'
-            );
+        emit SwapCreated(
+            swapId,
+            newSwap.initiatorAddress,
+            newSwap.responderAddress
+        );
+    }
+    // Counter the swap offer. 
+    function counterSwap(
+        string calldata swapId,
+        Asset[] memory initiatorAssets,
+        Asset[] memory responderAssets        
+    ) public payable {
+        //Fetch an existing swap and verify it contains data
+        Swap storage existingSwap = swaps[swapId];
+        require(bytes(existingSwap.swapId).length > 0, 'A swap with this id does not exist');
 
-            _validateAssetsBeforeTransfer(
-                targetSwap.initiatorAssets,
-                initiatorAssets
-            );
-            _validateAssetsBeforeTransfer(
-                targetSwap.responderAssets,
-                responderAssets
-            );
-
-            for (uint i = 0; i < targetSwap.initiatorAssets.length; i++) {
-                _transferAssets(
-                    targetSwap.initiatorAddress,
-                    targetSwap.responderAddress,
-                    targetSwap.initiatorAssets[i].assetAddress,
-                    targetSwap.initiatorAssets[i].value
-                );
-            }
-
-            for (uint i = 0; i < targetSwap.responderAssets.length; i++) {
-                _transferAssets(
-                    targetSwap.responderAddress,
-                    targetSwap.initiatorAddress,
-                    targetSwap.responderAssets[i].assetAddress,
-                    targetSwap.responderAssets[i].value
-                );
-            }
-
-            targetSwap.responderApprove = true;
-            targetSwap.status = 'COMPLETED';
-
-            emit SwapCompleted(
-                swapId,
-                targetSwap.initiatorAddress,
-                targetSwap.responderAddress
-            );
-        } else {
-            require(bytes(swapType).length > 0, 'Swap type is missing');
-            require(
-                keccak256(bytes(swapType)) == keccak256(bytes('OPEN')) ||
-                    keccak256(bytes(swapType)) == keccak256(bytes('PRIVATE')),
-                'Provided swap type is invalid'
-            );
-
-            targetSwap.swapId = swapId;
-            targetSwap.initiatorAddress = msg.sender;
-            targetSwap.responderAddress = responderAddress;
-            targetSwap.initiatorApprove = true;
-            targetSwap.responderApprove = false;
-            targetSwap.status = 'PENDING';
-            targetSwap.swapType = swapType;
-
-            for (uint i = 0; i < initiatorAssets.length; i++) {
-                targetSwap.initiatorAssets.push(
-                    Asset({
-                        assetAddress: initiatorAssets[i].assetAddress,
-                        value: initiatorAssets[i].value
-                    })
-                );
-            }
-
-            for (uint i = 0; i < responderAssets.length; i++) {
-                targetSwap.responderAssets.push(
-                    Asset({
-                        assetAddress: responderAssets[i].assetAddress,
-                        value: responderAssets[i].value
-                    })
-                );
-            }
-
-            emit SwapCreated(
-                swapId,
-                targetSwap.initiatorAddress,
-                targetSwap.responderAddress
+        require(
+            msg.sender == existingSwap.responderAddress,
+            'Only responder is allowed to counter a swap offer'
+        );
+        require(
+            keccak256(bytes(existingSwap.status)) ==
+                keccak256(bytes('PENDING')),
+            'Swap is not pending'
+        );
+        
+        //change the swap parties for this swap
+        existingSwap.responderAddress = existingSwap.initiatorAddress;
+        existingSwap.initiatorAddress = msg.sender;
+        for (uint i = 0; i < existingSwap.initiatorAssets.length; i++){
+            existingSwap.initiatorAssets.pop();
+        }
+        for (uint i = 0; i < existingSwap.responderAssets.length; i++){
+            existingSwap.responderAssets.pop();
+        }
+        for (uint i = 0; i < initiatorAssets.length; i++) {
+            existingSwap.initiatorAssets.push(
+                Asset({
+                    assetAddress: initiatorAssets[i].assetAddress,
+                    value: initiatorAssets[i].value
+                })
             );
         }
-    }
+        for (uint i = 0; i < responderAssets.length; i++) {
+            existingSwap.responderAssets.push(
+                Asset({
+                    assetAddress: responderAssets[i].assetAddress,
+                    value: responderAssets[i].value
+                })
+            );
+        }
 
-    function _validateAssetsBeforeTransfer(
-        Asset[] storage swapPartyAssets,
-        Asset[] calldata actualPartyAssets
-    ) private view {
-        require(swapPartyAssets.length > 0, 'No assets provided');
-        require(actualPartyAssets.length > 0, 'No assets provided');
+        emit SwapCountered(
+            swapId,
+            existingSwap.initiatorAddress,
+            existingSwap.responderAddress
+        );
+    }
+    //complete the swap offer, either as accept or reject
+    function completeSwap(
+        string calldata swapId,       
+        Asset[] memory initiatorAssets,
+        Asset[] memory responderAssets,        
+        string memory swapStatus
+    ) public payable {
+        //Fetch an existing swap and verify it contains data
+        Swap storage existingSwap = swaps[swapId];
+        require(bytes(existingSwap.swapId).length > 0, 'A swap with this id does not exist');
+
         require(
-            actualPartyAssets.length == swapPartyAssets.length,
-            'Invalid assets provided'
+            msg.sender == existingSwap.responderAddress,
+            'Only responder is allowed to counter a swap offer'
+        );
+        require(
+            keccak256(bytes(existingSwap.status)) ==
+                keccak256(bytes('PENDING')),
+            'Swap is not pending'
         );
 
-        for (uint i = 0; i < actualPartyAssets.length; i++) {
-            require(
-                swapPartyAssets[i].assetAddress ==
-                    actualPartyAssets[i].assetAddress,
-                'Asset address is incorrect'
+        require(swapStatus.equal('COMPLETED') || swapStatus.equal('REJECTED'), 'Invalid swap status');
+        
+        existingSwap.status = swapStatus;
+        if(existingSwap.status.equal('REJECTED')) {
+            emit SwapCompleted(
+                swapId,
+                existingSwap.initiatorAddress,
+                existingSwap.responderAddress,
+                'REJECTED'
             );
-            require(
-                swapPartyAssets[i].value == actualPartyAssets[i].value,
-                'Asset value is incorrect'
+            return;
+        }
+
+        // _validateAssetsBeforeTransfer(
+        //     existingSwap.initiatorAssets,
+        //     initiatorAssets
+        // );
+        // _validateAssetsBeforeTransfer(
+        //     existingSwap.responderAssets,
+        //     responderAssets
+        // );
+
+        for (uint i = 0; i < existingSwap.initiatorAssets.length; i++) {
+            _transferAssets(
+                existingSwap.initiatorAddress,
+                existingSwap.responderAddress,
+                existingSwap.initiatorAssets[i].assetAddress,
+                existingSwap.initiatorAssets[i].value
             );
         }
+
+        for (uint i = 0; i < existingSwap.responderAssets.length; i++) {
+            _transferAssets(
+                existingSwap.responderAddress,
+                existingSwap.initiatorAddress,
+                existingSwap.responderAssets[i].assetAddress,
+                existingSwap.responderAssets[i].value
+            );
+        }
+
+        existingSwap.responderApprove = true;       
+
+        emit SwapCompleted(
+            swapId,
+            existingSwap.initiatorAddress,
+            existingSwap.responderAddress,
+            'COMPLETED'
+        );
     }
+
+    // function _validateAssetsBeforeTransfer(
+    //     Asset[] memory swapPartyAssets,
+    //     Asset[] memory actualPartyAssets
+    // ) private pure {
+    //     require(swapPartyAssets.length > 0, 'No assets provided');
+    //     require(actualPartyAssets.length > 0, 'No assets provided');
+    //     require(
+    //         actualPartyAssets.length == swapPartyAssets.length,
+    //         'Invalid assets provided'
+    //     );
+
+    //     for (uint i = 0; i < actualPartyAssets.length; i++) {
+    //         require(
+    //             swapPartyAssets[i].assetAddress ==
+    //                 actualPartyAssets[i].assetAddress,
+    //             'Asset address is incorrect'
+    //         );
+    //         require(
+    //             swapPartyAssets[i].value == actualPartyAssets[i].value,
+    //             'Asset value is incorrect'
+    //         );
+    //     }
+    // }
 
     // Function to swap tokens from the contract address to the recipient address
     function transferTokens(
